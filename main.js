e6e906d9d462bef5b50eb2a5ef963614466ac322
Hang Vuong <vuonghalyn@gmail.com>
2022-05-18 21:03:30 +0300

    add game logic

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 // Global stage section
-// const modalScreen = document.querySelector("#letsPlay");
-
 const resultText = document.querySelector("#announcement");
+const popUp = document.querySelector("#myModal");
+const finalText = document.querySelector("#finalText");
+const restartGame = document.querySelector("#restartGame");
 
-const scoreEachRound = [];
+let scoreEachRound = [];
 
 let isUserLosing = false;
 let isComputerLosing = false;
@@ -11,62 +12,81 @@ let isComputerLosing = false;
 const playerHand = document.querySelector(".player-hand");
 const computerHand = document.querySelector(".computer-hand");
 
-// Random Number
-// Scoring
-// Display the result
+const introScreen = document.querySelector(".intro");
+const match = document.querySelector(".match");
 
-// modalScreen.onclick = () => {
-//   div.fadeOut;
-// };
+let userScore = 0;
+const userScoreDisplay = document.querySelector("#currentUserScore");
 
-// Start game by pushing Let's Play
-const game = () => {
-  let userScore = 0;
-  const userScoreDisplay = document.querySelector("#currentUserScore");
+let computerScore = 0;
+const computerScoreDisplay = document.querySelector("#currentComputerScore");
 
-  let computerScore = 0;
-  const computerScoreDisplay = document.querySelector("#currentComputerScore");
+let userRoundResult;
 
-  const startingGame = () => {
-    const playButtonHandler = document.querySelector(".intro button");
-    const introScreen = document.querySelector(".intro");
-    const match = document.querySelector(".match");
+const pushPlaybtnToStart = () => {
+  const playButtonHandler = document.querySelector(".intro button");
 
-    playButtonHandler.addEventListener("click", () => {
-      introScreen.classList.add("fadeOut");
-      match.classList.add("fadeIn");
-    });
-  };
-  startingGame();
-  // get User Choice
-  const startGame = (event) => {
-    let userRoundResult;
-    let userChoice = event.target.id;
-    if (userChoice == "rock") {
-      userChoice = 0;
-      playerHand.src = "./Assets/rock.png";
-    } else if (userChoice == "paper") {
-      userChoice = 1;
-      playerHand.src = "./Assets/paper.png";
-    } else {
-      userChoice = 2;
-      playerHand.src = "./Assets/scissors.png";
-    }
+  playButtonHandler.addEventListener("click", () => {
+    introScreen.classList.add("fadeOut");
+    match.classList.add("fadeIn");
+  });
+};
+pushPlaybtnToStart();
 
-    // get Computer Choice
-    // 0 = rock
-    // 1 = paper
-    // 2 = scissors
-    const computerChoice = Math.floor(Math.random() * 3);
+const clickButtonToClose = () => {
+  restartGame.addEventListener("click", () => {
+    popUp.style.display = "none";
+    introScreen.classList.remove("fadeOut");
+    introScreen.classList.add("fadeIn");
+    match.classList.remove("fadeIn");
+    match.classList.add("fadeOut");
+    scoreEachRound = [];
+    isComputerLosing = false;
+    isUserLosing = false;
+    userScore = 0;
+    computerScore = 0;
+    userRoundResult = null;
+    userScoreDisplay.innerHTML = userScore;
+    computerScoreDisplay.innerHTML = computerScore;
+    resultText.innerHTML = "";
+  });
+};
+clickButtonToClose();
+// 0 = rock
+// 1 = paper
+// 2 = scissors
 
-    if (computerChoice == 0) {
-      computerHand.src = "./Assets/rock.png";
-    } else if (computerChoice == 1) {
-      computerHand.src = "./Assets/paper.png";
-    } else {
-      computerHand.src = "./Assets/scissors.png";
-    }
+// get User Choice
+const startGame = (event) => {
+  let userChoice = event.target.id;
 
+  if (userChoice == "rock") {
+    userChoice = 0;
+    playerHand.src = "./Assets/rock.png";
+  } else if (userChoice == "paper") {
+    userChoice = 1;
+    playerHand.src = "./Assets/paper.png";
+  } else {
+    userChoice = 2;
+    playerHand.src = "./Assets/scissors.png";
+  }
+
+  // get Computer Choice
+
+  const computerChoice = Math.floor(Math.random() * 3);
+  console.log(computerChoice);
+
+  if (computerChoice == 0) {
+    computerHand.src = "./Assets/rock.png";
+  } else if (computerChoice == 1) {
+    computerHand.src = "./Assets/paper.png";
+  } else {
+    computerHand.src = "./Assets/scissors.png";
+  }
+  playerHand.style.animation = "shakePlayer 2s ease";
+  computerHand.style.animation = "shakeComputer 2s ease";
+
+  const compareScore = () => {
     // Compare result each round and plus score for the winner
     if (userChoice == 0 && computerChoice == 2) {
       console.log("Result: Player Win with Rock");
@@ -74,6 +94,8 @@ const game = () => {
       resultText.innerHTML = "You win";
       userRoundResult = "win";
       console.log(userScore);
+      userScoreDisplay.innerHTML = userScore;
+      userScoreDisplay;
       scoreEachRound.push({
         userScore: userScore,
         computerScore: computerScore,
@@ -82,9 +104,10 @@ const game = () => {
     } else if (userChoice == 1 && computerChoice == 0) {
       console.log("Result: Player Win with Paper");
       userScore++;
+      userScoreDisplay.innerHTML = userScore;
       resultText.innerHTML = "You win";
-      console.log(userScore);
-      console.log(scoreEachRound);
+      console.log("userScore: ", userScore);
+      console.log("scoreEachRound: ", scoreEachRound);
       userRoundResult = "win";
 
       scoreEachRound.push({
@@ -96,6 +119,7 @@ const game = () => {
       console.log("Result: Player Win with Scissors");
       userScore++;
       resultText.innerHTML = "You win";
+      userScoreDisplay.innerHTML = userScore;
       console.log(userScore);
       console.log(scoreEachRound);
       userRoundResult = "win";
@@ -120,6 +144,7 @@ const game = () => {
     } else {
       console.log("You lose");
       computerScore++;
+      computerScoreDisplay.innerHTML = computerScore;
       resultText.innerHTML = "You lose";
       console.log(computerScore);
 
@@ -132,12 +157,9 @@ const game = () => {
         userRoundResult: userRoundResult,
       });
     }
+  };
 
-    //dislay score to the game
-    userScoreDisplay.innerHTML = userScore;
-    computerScoreDisplay.innerHTML = computerScore;
-
-    //check if 3 win in a row or 3 lose in a row
+  const endingGameWith3StraightWins = () => {
     if (scoreEachRound.length >= 3) {
       console.log(scoreEachRound[scoreEachRound.length - 1].userRoundResult);
       console.log(scoreEachRound[scoreEachRound.length - 2].userRoundResult);
@@ -164,6 +186,8 @@ const game = () => {
         scoreEachRound[scoreEachRound.length - 1].userRoundResult == "win"
       ) {
         console.log("user win whole game with 3 straight win");
+        popUp.style.display = "block";
+        finalText.innerHTML = "You won";
       }
       if (
         scoreEachRound[scoreEachRound.length - 1].userRoundResult ==
@@ -173,48 +197,36 @@ const game = () => {
         scoreEachRound[scoreEachRound.length - 1].userRoundResult == "lose"
       ) {
         console.log("user lose whole game with 3 straight lost");
+        popUp.style.display = "block";
+        finalText.innerHTML = "You lost";
       }
-
-      // if (
-      //   scoreEachRound[i].userScore == scoreEachRound[i - 1].userScore &&
-      //   scoreEachRound[i].computerScore == scoreEachRound[i - 1].computerScore
-      // ) {
-      //   isUserLosing = false;
-      //   isComputerLosing = false;
-      //   break;
-      // }
-      // if (scoreEachRound[i].userScore == scoreEachRound[i - 1].userScore) {
-      //   isUserLosing = true;
-      // } else {
-      //   isUserLosing = false;
-      //   break;
-      // }
-
-      // if (
-      //   scoreEachRound[i].computerScore == scoreEachRound[i - 1].computerScore
-      // ) {
-      //   isComputerLosing = true;
-      // } else {
-      //   isComputerLosing = false;
-      //   break;
-      // }
-      // console.log("run");
 
       console.log(isComputerLosing);
       console.log(isUserLosing);
-
-      //check who gets to 10 points first
-      if (userScore == 10 && computerScore <= 10) {
-        console.log("Compare 10 points: user win whole game");
-      }
-      if (userScore <= 10 && computerScore == 10) {
-        console.log("Compare 10 points: user lose whole game");
-      }
     }
   };
+  const winWith10Points = () => {
+    if (userScore == 10 && computerScore <= 10) {
+      console.log("Compare 10 points: user win whole game");
+      popUp.style.display = "block";
+      finalText.innerHTML = "You won";
+      return;
+    }
+    if (userScore <= 10 && computerScore == 10) {
+      console.log("Compare 10 points: user lose whole game");
+      popUp.style.display = "block";
+      finalText.innerHTML = "You lost";
+      return;
+    }
+  };
+  setTimeout(() => {
+    compareScore();
+    winWith10Points();
+    endingGameWith3StraightWins();
+    playerHand.style.animation = "";
+    computerHand.style.animation = "";
+  }, 2000);
 };
-game();
-startGame();
 
 const rockButton = document.getElementById("rock");
 rockButton.addEventListener("click", startGame);
@@ -224,3 +236,45 @@ paperButton.addEventListener("click", startGame);
 
 const scissorsButton = document.getElementById("scissors");
 scissorsButton.addEventListener("click", startGame);
+
+//dislay score to the game
+
+computerScoreDisplay.innerHTML = computerScore;
+
+//   //check if 3 win in a row or 3 lose in a row
+
+//check who gets to 10 points first
+
+// const rockButton = document.getElementById("rock");
+// rockButton.addEventListener("click", startGame);
+
+// const paperButton = document.getElementById("paper");
+// paperButton.addEventListener("click", startGame);
+
+// const scissorsButton = document.getElementById("scissors");
+// scissorsButton.addEventListener("click", startGame);
+
+// if (
+//   scoreEachRound[i].userScore == scoreEachRound[i - 1].userScore &&
+//   scoreEachRound[i].computerScore == scoreEachRound[i - 1].computerScore
+// ) {
+//   isUserLosing = false;
+//   isComputerLosing = false;
+//   break;
+// }
+// if (scoreEachRound[i].userScore == scoreEachRound[i - 1].userScore) {
+//   isUserLosing = true;
+// } else {
+//   isUserLosing = false;
+//   break;
+// }
+
+// if (
+//   scoreEachRound[i].computerScore == scoreEachRound[i - 1].computerScore
+// ) {
+//   isComputerLosing = true;
+// } else {
+//   isComputerLosing = false;
+//   break;
+// }
+// console.log("run");
